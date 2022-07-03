a0ed40b461eaf28f3011ccc4dd87eccdf32fd64c
arghachdas <arghachandra@technoexponent.com>
2022-07-03 12:36:55 +0530

    Activate account api endpoint completed

--- controllers/user.js
+++ controllers/user.js
@@ -7,6 +7,7 @@ const User = require("../models/user")
 const bcrypt = require("bcrypt")
 const { generateToken } = require("../helpers/tokens")
 const { sendVerificationEmail } = require("../helpers/mailer")
+const jwt = require("jsonwebtoken")
 
 exports.register = async (req, res) => {
   try {
@@ -87,5 +88,21 @@ exports.register = async (req, res) => {
     })
   } catch (error) {
     res.status(500).json({ message: error.message })
+  }
+}
+
+exports.activateAccount = async (req, res) => {
+  const { token } = req.body
+  const user = jwt.verify(token, process.env.JWT_SECRET)
+  const check = await User.findById(user.id)
+  if (check.verified) {
+    return res
+      .status(400)
+      .json({ message: "This account is already activated" })
+  } else {
+    await User.findByIdAndUpdate(user.id, { verified: true })
+    return res
+      .status(200)
+      .json({ message: "Account has been activated successfully" })
   }
 }
