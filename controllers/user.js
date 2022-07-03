4f507a4bee68043d673382a55bdc6b178f6c1156
arghachdas <arghachandra@technoexponent.com>
2022-07-03 12:47:03 +0530

    login api completed

--- controllers/user.js
+++ controllers/user.js
@@ -92,17 +92,53 @@ exports.register = async (req, res) => {
 }
 
 exports.activateAccount = async (req, res) => {
-  const { token } = req.body
-  const user = jwt.verify(token, process.env.JWT_SECRET)
-  const check = await User.findById(user.id)
-  if (check.verified) {
-    return res
-      .status(400)
-      .json({ message: "This account is already activated" })
-  } else {
-    await User.findByIdAndUpdate(user.id, { verified: true })
-    return res
-      .status(200)
-      .json({ message: "Account has been activated successfully" })
+  try {
+    const { token } = req.body
+    const user = jwt.verify(token, process.env.JWT_SECRET)
+    const check = await User.findById(user.id)
+    if (check.verified) {
+      return res
+        .status(400)
+        .json({ message: "This account is already activated" })
+    } else {
+      await User.findByIdAndUpdate(user.id, { verified: true })
+      return res
+        .status(200)
+        .json({ message: "Account has been activated successfully" })
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+    if (!user)
+      return res.status(400).json({
+        message:
+          "This email address you entered in not connected to an account",
+      })
+
+    const check = await bcrypt.compare(password, user.password)
+    if (!check)
+      return res.status(400).json({
+        message: "Invalid credentials. Please try again",
+      })
+
+    const token = generateToken({ id: user._id.toString() }, "7d")
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      token,
+      verified: user.verified,
+      message: "Logged in successfully",
+    })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
   }
 }
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express")
-const { register, activateAccount } = require("../controllers/user")
+const { register, activateAccount, login } = require("../controllers/user")
 
 const router = express.Router()
 
 router.post("/register", register)
+router.post("/login", login)
 router.post("/activate", activateAccount)
 
 module.exports = router
